16268a79810dd5e879bb56c71222709692e602c4
LukaszK1983 <luke83@poczta.fm>
2021-02-24 16:36:00 +0100

    loan and mortgage upgrade

--- src/main/java/pl/wybankuj/web/converter/BigDecimalConverter.java
+++ src/main/java/pl/wybankuj/web/converter/BigDecimalConverter.java
@@ -6,6 +6,7 @@ import org.springframework.core.convert.converter.GenericConverter;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.Set;
 
 @Component
@@ -33,7 +34,8 @@ public class BigDecimalConverter implements GenericConverter {
         } else {
             Number number = (Number) source;
             BigDecimal converted = BigDecimal.valueOf(number.doubleValue());
-            return converted.setScale(2, BigDecimal.ROUND_HALF_EVEN);
+            return converted.setScale(2, RoundingMode.HALF_EVEN);
+//            return converted.setScale(2, BigDecimal.ROUND_HALF_EVEN);
         }
     }
 }
